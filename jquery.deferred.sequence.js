e5608aeb688056d4bed1c08239c483597da6c037
Kyle Florence <kyle.florence@gmail.com>
2016-12-31 14:58:53 -0800

    Publish to NPM

--- jquery.deferred.sequence.js
+++ jquery.deferred.sequence.js
@@ -1,60 +1,60 @@
-(function( factory ) {
-    // AMD
-    if ( typeof define === "function" && define.amd ) {
-        define( [ "jquery" ], factory);
+(function (factory) {
+  // AMD
+  if (typeof define === "function" && define.amd) {
+    define(["jquery"], factory);
 
     // Node/CommonJS
-    } else if ( typeof exports === "object" ) {
-        factory( require( "jquery" ) );
+  } else if (typeof exports === "object") {
+    factory(require("jquery"));
 
     // Browser
-    } else {
-        factory( jQuery );
-    }
-}(function( $ ) {
-    var slice = Array.prototype.slice;
+  } else {
+    factory(jQuery);
+  }
+}(function ($) {
+  var slice = Array.prototype.slice;
 
-    function scopedFunc( func ) {
-        var args = slice.call( arguments, 1 );
+  function scopedFunc(func) {
+    var args = slice.call(arguments, 1);
 
-        return function() {
-            return func.apply( this, args.concat( slice.call( arguments ) ) );
-        };
-    }
+    return function () {
+      return func.apply(this, args.concat(slice.call(arguments)));
+    };
+  }
 
-    function Sequence( items ) {
-        var head = $.Deferred(),
-            master = $.Deferred(),
-            tail = head;
+  function Sequence(items) {
+    var head = $.Deferred(),
+      master = $.Deferred(),
+      tail = head;
 
-        items = $.makeArray( items );
+    items = $.makeArray(items);
 
-        return {
-            head: head,
-            items: items,
-            master: master,
-            reduce: function( value, func, context ) {
-                // Args: func, context
-                if ( typeof value === "function" ) {
-                    context = func;
-                    func = value;
-                    value = undefined;
-                }
+    return {
+      head: head,
+      items: items,
+      master: master,
+      reduce: function (value, func, context) {
+        // Args: func, context
+        if (typeof value === "function") {
+          context = func;
+          func = value;
+          value = undefined;
+        }
 
-                head.resolveWith( context, $.makeArray( value ) );
+        head.resolveWith(context, $.makeArray(value));
 
-                $.each( items, function( i, item ) {
-                    tail = tail.pipe( scopedFunc( func, item ) );
-                });
+        $.each(items, function (i, item) {
+          tail = tail.pipe(scopedFunc(func, item));
+        });
 
-                tail.done( scopedFunc( master.resolve ) );
+        tail.done(scopedFunc(master.resolve));
 
-                return master;
-            },
-            tail: tail
-        };
-    }
+        return master;
+      },
+      tail: tail
+    };
+  }
 
-    // Exports
-    $.Deferred.Sequence = Sequence;
+  // Exports
+  $.Deferred.Sequence = Sequence;
 }));
